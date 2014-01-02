ea3b8fa50ec602769e955f8b9f3fb2f7ec9f9ea6
Jeff Anderson <jefferya@programmerq.net>
2014-01-01 18:30:14 -0700

    we can now calculate the next state

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,21 @@
 class Cell
-  def initialize(alive: true)
+  def initialize(board, alive: true)
     @alive=alive
+    @board=board
   end
   def alive?
     return @alive
+  end
+  def get_neighbors
+    @board.neighbors(self)
+  end
+  def calculate_next_state
+    alive_neighbors = get_neighbors.select { |c| c.alive? }.count
+    if self.alive?
+      return true if alive_neighbors.between?(2,3)
+    else
+      return true if alive_neighbors.eql? 3
+    end
+    return false
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,8 +1,57 @@
 require 'spec_helper'
 
 describe Cell do
-  let (:cell1) { Cell.new() }
-  it 'creates a cell instance' do
+  let (:myboard) { double('brd', :neighbors => [double(Cell)]) }
+  let (:cell1) { Cell.new(myboard) }
+  let (:cell2) { Cell.new(myboard, alive: false) }
+  let (:neighbor_possibilities) { [
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false)],
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false)],
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false)],
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false)],
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false)],
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: false), Cell.new(myboard, alive: false)],
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: false)],
+      [Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true), Cell.new(myboard, alive: true)],
+  ] }
+
+  it '.alive?' do
     expect(cell1.alive?).to eq(true)
+    expect(cell2.alive?).to eq(false)
+    #cell2.alive = true
+    #expect(cell2.alive?).to eq(true)
   end
+  
+  it 'finds its neighbors' do
+    expect(myboard).to receive('neighbors').with(cell1)
+    cell1.get_neighbors
+  end
+
+  context 'with an already live cell' do
+    it '.calculate_next_state' do
+      allow(cell1).to receive(:get_neighbors).and_return(*neighbor_possibilities)
+      expect(cell1.calculate_next_state).to eq(false)
+      expect(cell1.calculate_next_state).to eq(true)
+      expect(cell1.calculate_next_state).to eq(true)
+      expect(cell1.calculate_next_state).to eq(false)
+      expect(cell1.calculate_next_state).to eq(false)
+      expect(cell1.calculate_next_state).to eq(false)
+      expect(cell1.calculate_next_state).to eq(false)
+      expect(cell1.calculate_next_state).to eq(false)
+    end
+  end
+  context 'with an already deade cell' do
+    it '.calculate_next_state' do
+      allow(cell2).to receive(:get_neighbors).and_return(*neighbor_possibilities)
+      expect(cell2.calculate_next_state).to eq(false)
+      expect(cell2.calculate_next_state).to eq(false)
+      expect(cell2.calculate_next_state).to eq(true)
+      expect(cell2.calculate_next_state).to eq(false)
+      expect(cell2.calculate_next_state).to eq(false)
+      expect(cell2.calculate_next_state).to eq(false)
+      expect(cell2.calculate_next_state).to eq(false)
+      expect(cell2.calculate_next_state).to eq(false)
+    end
+  end
+
 end
