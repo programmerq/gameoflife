002dccebb67572f2ee2710cf61805814ca021ef0
Jeff Anderson <programmerq@gmail.com>
2014-01-02 22:05:59 -0700

    super basic initial test and minimal bogus code to make the test pass

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,11 @@
 class Board
+  def initialize (size)
+    
+  end
+  def [](key)
+    Array.new 4
+  end
+  def length
+    5
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Board do
-
+  let (:my_board_small) { Board.new([4,5]) }
+  it 'creates a board of the correct size' do
+    expect(my_board_small.length).to eql 5
+    expect(my_board_small[0].length).to eql 4
+  end
 end
